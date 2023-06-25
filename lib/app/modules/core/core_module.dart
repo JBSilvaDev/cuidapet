9baeb1fefb6b492f281bd05a495df33eb3888ee1
JBSilvaDev <jbsilva.dev@outlook.com>
2023-06-25 04:39:26 -0300

    encapsulando Dio

--- lib/app/modules/core/core_module.dart
+++ lib/app/modules/core/core_module.dart
@@ -1,3 +1,5 @@
+import 'package:app_cuida_pet/app/core/rest_client/dio_rest_client.dart';
+import 'package:app_cuida_pet/app/core/rest_client/rest_client.dart';
 import 'package:app_cuida_pet/app/modules/core/auth/auth_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -5,7 +7,8 @@ class CoreModule extends Module {
 
    @override
    List<Bind> get binds => [
-    Bind.lazySingleton((i) => AuthStore(), export: true)
+    Bind.lazySingleton((i) => AuthStore(), export: true),
+    Bind.lazySingleton<RestClient>((i) => DioRestClient(), export: true)
    ];
 
 
